b0336d368acb17dc7f978f4e3b0ea860d1e2877c
Tart123 <artaayr@gmail.com>
2022-08-25 11:53:52 -0600

    Initial commit

---
+++ src/main/java/org/example/NodoBinario.java
@@ -0,0 +1,74 @@
+package org.example;
+
+public class NodoBinario<S> {
+    private String element;
+    private NodoBinario<String> izquierdo;
+    private NodoBinario<String> derecho;
+
+
+    //Contructores
+    public NodoBinario(){
+        this(null, null, null);
+    }
+    public NodoBinario(String elemento, NodoBinario<String> izq,NodoBinario<String> der){
+        element = elemento;
+        izquierdo = izq;
+        derecho = der;
+
+    }
+
+
+    //Métodos accesores y mutadores
+    public String getElement() {
+        return element;
+    }
+
+    public void setElement(String element) {
+        this.element = element;
+    }
+
+    public NodoBinario<String> getIzquierdo() {
+        return izquierdo;
+    }
+
+    public void setIzquierdo(NodoBinario<String> izquierdo) {
+        this.izquierdo = izquierdo;
+    }
+
+    public NodoBinario<String> getDerecho() {
+        return derecho;
+    }
+
+    public void setDerecho(NodoBinario<String> derecho) {
+        this.derecho = derecho;
+    }
+
+    //Métodos para obtener datos del arbol
+    public static int tamano(NodoBinario<String> t){
+
+        return 0;
+    }
+
+    public static int altura(){
+
+        return 0;
+    }
+
+    public NodoBinario<String> duplicar(){
+
+        return null;
+    }
+
+    //Imprimir los arboles binarios
+    public void printPreOrden(){
+
+    }
+
+    public void printPostOrden(){
+
+    }
+
+    public void printInOrden(){
+
+    }
+}
